c8bfc17984cf4615d0a291155dd2aa2dcdab6c7b
Alexander Gavrilov <108229249+AlexandrReactJs@users.noreply.github.com>
2022-11-11 18:16:21 +0300

    change

--- frontend/src/Redux/Slices/AdminSlice.js
+++ frontend/src/Redux/Slices/AdminSlice.js
@@ -2,7 +2,12 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   adminData: null,
-  isAuth: false
+  isAuth: false,
+  addPostData: {
+    title: 'asd',
+    text: null
+  }
+
 }
 
 export const adminSlice = createSlice({
@@ -14,12 +19,19 @@ export const adminSlice = createSlice({
    },
    setIsAuth: (state, action) => {
     state.isAuth = action.payload
+   },
+
+   setPostTitle: (state, action) => {
+    state.addPostData.title = action.payload.title
+   },
+   setPostText: (state, action) => {
+    state.addPostData.text = action.payload.text
    }
    
   },
 })
 
 
-export const { setAdminData, setIsAuth } = adminSlice.actions
+export const { setAdminData, setIsAuth, setPostTitle, setPostText } = adminSlice.actions
 
 export default adminSlice.reducer
--- frontend/src/components/Admin/AddPost/AddPost.jsx
+++ frontend/src/components/Admin/AddPost/AddPost.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
+import styles from './AddPost.module.css';
+import { setPostText, setPostTitle } from '../../../Redux/Slices/AdminSlice';
+
 
 const AddPost = () => {
+    const dispatch = useDispatch();
     const JWTToken = useSelector(state => state.adminSlice.adminData.token)
+    const titleValue = useSelector(state => state.adminSlice.addPostData.title)
+
+    const titleRef = React.useRef(null)
 
     const addPost = (body) => {
         axios.post('http://localhost:4444/posts', body, { headers: { Authorization: "Bearer " + JWTToken } }).then((res) => {
@@ -15,14 +22,22 @@ const AddPost = () => {
     
     }
 
+    const onChangeTitle = (event) => {
+        const text = titleRef.current.value;
+        dispatch(setPostTitle(text))
+    }
+
+
+
     return (
-        <div>
-            <div>
-                <textarea/>
+        <div className={styles.addPost}>
+            <div className={styles.addTitle}>
+                <textarea ref={titleRef} value={titleValue} onChange= {onChangeTitle} placeholder='Добавить заголовок'/>
             </div>
-            <div>
-                <textarea/>
+            <div className={styles.addText}>
+                <textarea placeholder='Добавить текст'/>
             </div>
+            <button></button>
         </div>
     )
 }
